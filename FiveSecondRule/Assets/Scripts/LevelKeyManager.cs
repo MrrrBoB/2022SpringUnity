e1715d163d05e2b449e9d525f370cfdaf0e33561
Nate <Nathoward22@gmail.com>
2022-03-26 17:36:49 -0600

    Created empty key manager with no functionality, pushed to phone

--- FiveSecondRule/Assets/Scripts/LevelKeyManager.cs
+++ FiveSecondRule/Assets/Scripts/LevelKeyManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelKeyManager : MonoBehaviour
+[CreateAssetMenu]
+public class LevelKeyManager : ScriptableObject
 {
+    public bool[] levelKeys;
     // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
         
     }
