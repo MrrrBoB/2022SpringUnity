d037420ba59a26d61a9110090e8dc58fcba3a099
Nate <Nathoward22@gmail.com>
2022-04-09 17:37:07 -0600

    Added Bedroom levels and hooked up the menu

---
+++ FiveSecondRule/Assets/Scripts/KeyManagerLevelStar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyManagerLevelStar : MonoBehaviour
+{
+    public LevelKeyManager mgr;
+
+    public int LevelKeyIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
